7f52baa932f9ee306a3bd732f470939eec2e658b
beats <hugoroblesmourino@gmail.com>
2023-03-07 16:02:39 +0100

    initial commit

---
+++ app/src/main/java/com/example/pipati/ModoJuego.java
@@ -0,0 +1,14 @@
+package com.example.pipati;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModoJuego extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modo_juego);
+    }
+}
